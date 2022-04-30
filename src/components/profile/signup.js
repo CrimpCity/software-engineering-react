347a5035083b458048c06ae8b832f63e57559306
CrimpCity <tutuianu.georgian@gmail.com>
2022-04-30 19:20:40 -0400

    format profile signup

--- src/components/profile/signup.js
+++ src/components/profile/signup.js
@@ -1,6 +1,6 @@
-import {useState} from "react";
+import { useState } from "react";
 import * as service from "../../services/security-service";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
     const [newUser, setNewUser] = useState({});
@@ -13,19 +13,19 @@ const Signup = () => {
         <div>
             <h1>Signup</h1>
             <input className="mb-2 form-control"
-                   onChange={(e) =>
-                       setNewUser({...newUser, username: e.target.value})}
-                   placeholder="username"/>
+                onChange={(e) =>
+                    setNewUser({ ...newUser, username: e.target.value })}
+                placeholder="username" />
             <input className="mb-2 form-control"
-                   onChange={(e) =>
-                       setNewUser({...newUser, password: e.target.value})}
-                   placeholder="password" type="password"/>
+                onChange={(e) =>
+                    setNewUser({ ...newUser, password: e.target.value })}
+                placeholder="password" type="password" />
             <input className="mb-2 form-control"
-                   onChange={(e) =>
-                       setNewUser({...newUser, email: e.target.value})}
-                   placeholder="email" type="email"/>
+                onChange={(e) =>
+                    setNewUser({ ...newUser, email: e.target.value })}
+                placeholder="email" type="email" />
             <button onClick={signup}
-                    className="btn btn-primary mb-5">Signup
+                className="btn btn-primary mb-5">Signup
             </button>
         </div>
     );
